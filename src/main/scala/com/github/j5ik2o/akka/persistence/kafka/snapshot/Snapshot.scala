744f17554c33cbc1db07ae49d1b930594e84ae4c
Junichi Kato <j5ik2o@gmail.com>
2020-03-18 23:16:09 +0900

    Add final.

--- src/main/scala/com/github/j5ik2o/akka/persistence/kafka/snapshot/Snapshot.scala
+++ src/main/scala/com/github/j5ik2o/akka/persistence/kafka/snapshot/Snapshot.scala
@@ -2,7 +2,7 @@ package com.github.j5ik2o.akka.persistence.kafka.snapshot
 
 import akka.persistence.{ SnapshotMetadata, SnapshotSelectionCriteria }
 
-case class Snapshot(metadata: SnapshotMetadata, payload: Any) {
+final case class Snapshot(metadata: SnapshotMetadata, payload: Any) {
   def matches(criteria: SnapshotSelectionCriteria): Boolean =
     metadata.sequenceNr <= criteria.maxSequenceNr &&
     metadata.timestamp <= criteria.maxTimestamp
