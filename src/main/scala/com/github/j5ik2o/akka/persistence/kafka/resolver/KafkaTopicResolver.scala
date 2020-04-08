51d2c663fb9c0a46e721ffef5c1cb36e93f182ce
Junichi Kato <j5ik2o@gmail.com>
2020-04-09 04:15:40 +0900

    fix resolver interface

--- src/main/scala/com/github/j5ik2o/akka/persistence/kafka/resolver/KafkaTopicResolver.scala
+++ src/main/scala/com/github/j5ik2o/akka/persistence/kafka/resolver/KafkaTopicResolver.scala
@@ -1,6 +1,7 @@
 package com.github.j5ik2o.akka.persistence.kafka.resolver
 
 import com.github.j5ik2o.akka.persistence.kafka.journal.{ PersistenceId => PID }
+import com.typesafe.config.Config
 
 trait KafkaTopicResolver {
 
@@ -10,7 +11,7 @@ trait KafkaTopicResolver {
 
 object KafkaTopicResolver {
 
-  class PersistenceId extends KafkaTopicResolver {
+  class PersistenceId(config: Config) extends KafkaTopicResolver {
     override def resolve(persistenceId: PID): KafkaTopic = {
       KafkaTopic(persistenceId.asString)
     }
