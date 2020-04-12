ab6ab8fc518c58d2671b0b4cbe29e29db8c89786
Junichi Kato <j5ik2o@gmail.com>
2020-04-12 21:06:47 +0900

    add support for async serializer with encryption

--- src/main/scala/com/github/j5ik2o/akka/persistence/kafka/serialization/JournalAkkaSerializer.scala
+++ src/main/scala/com/github/j5ik2o/akka/persistence/kafka/serialization/JournalAkkaSerializer.scala
@@ -2,51 +2,72 @@ package com.github.j5ik2o.akka.persistence.kafka.serialization
 
 import akka.actor.ExtendedActorSystem
 import akka.persistence.PersistentRepr
-import akka.serialization.{ SerializationExtension, Serializer => AkkaSerializer }
+import akka.serialization.{ AsyncSerializerWithStringManifest, SerializationExtension }
 import com.github.j5ik2o.akka.persistence.kafka.journal.JournalRow
 import com.github.j5ik2o.akka.persistence.kafka.protocol.JournalFormat
 import com.google.protobuf.ByteString
+import com.typesafe.config.Config
 import org.slf4j.LoggerFactory
+
+import scala.collection.immutable
+import scala.concurrent.Future
 
 object JournalAkkaSerializer {
   val Identifier = 15443
 }
 
-class JournalAkkaSerializer(system: ExtendedActorSystem) extends AkkaSerializer {
-  private val logger = LoggerFactory.getLogger(getClass)
+class JournalAkkaSerializer(system: ExtendedActorSystem) extends AsyncSerializerWithStringManifest(system) {
+  import system.dispatcher
+  private val config: Config = system.settings.config.getConfig("j5ik2o.kafka-journal")
 
-  override def identifier: Int          = JournalAkkaSerializer.Identifier
-  override def includeManifest: Boolean = false
+  private val logger                       = LoggerFactory.getLogger(getClass)
+  override def identifier: Int             = JournalAkkaSerializer.Identifier
+  override def manifest(o: AnyRef): String = o.getClass.getName
 
-  override def toBinary(o: AnyRef): Array[Byte] = o match {
-    case journal: JournalRow =>
-      logger.debug("toBinary:journal = {}", journal)
-      val serializer = SerializationExtension(system).findSerializerFor(journal.persistentRepr)
-      JournalFormat(
-        persistenceId = journal.persistenceId.asString,
-        sequenceNumber = journal.sequenceNumber.value,
-        persistentRepr = ByteString.copyFrom(serializer.toBinary(journal.persistentRepr)),
-        deleted = journal.deleted,
-        manifest = journal.manifest,
-        timestamp = journal.timestamp,
-        writerUuid = journal.writerUuid,
-        tags = journal.tags
-      ).toByteArray
-    case _ => throw new IllegalArgumentException(s"Can't serialize object of type ${o.getClass}")
+  private val byteArrayFilter: EncryptionFilter = {
+    val className = config.getString("encryption-filter-class-name")
+    system.dynamicAccess
+      .createInstanceFor[EncryptionFilter](className, immutable.Seq.empty)
+      .getOrElse(throw new ClassNotFoundException(className))
   }
 
-  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
-    val journalFormat = JournalFormat.parseFrom(bytes)
-    val result = JournalRow(
-      persistentRepr = SerializationExtension(system)
-        .deserialize(
-          journalFormat.persistentRepr.toByteArray,
-          classOf[PersistentRepr]
+  override def toBinaryAsync(o: AnyRef): Future[Array[Byte]] = {
+    o match {
+      case journal: JournalRow =>
+        logger.debug("toBinary:journal = {}", journal)
+        Future {
+          val serializer = SerializationExtension(system).findSerializerFor(journal.persistentRepr)
+          JournalFormat(
+            persistenceId = journal.persistenceId.asString,
+            sequenceNumber = journal.sequenceNumber.value,
+            persistentRepr = ByteString.copyFrom(serializer.toBinary(journal.persistentRepr)),
+            deleted = journal.deleted,
+            manifest = journal.manifest,
+            timestamp = journal.timestamp,
+            writerUuid = journal.writerUuid,
+            tags = journal.tags
+          ).toByteArray
+        }.flatMap(
+          byteArrayFilter.encrypt(journal.persistenceId, _, Map("tags" -> journal.tags))
         )
-        .get,
-      tags = journalFormat.tags.toList
-    )
-    logger.debug("fromBinary:journal = {}", result)
-    result
+      case _ => Future.failed(new IllegalArgumentException(s"Can't serialize object of type ${o.getClass}"))
+    }
   }
+
+  override def fromBinaryAsync(bytes: Array[Byte], manifest: String): Future[AnyRef] =
+    byteArrayFilter.decrypt(bytes).map { decryptedBytes =>
+      val journalFormat = JournalFormat.parseFrom(decryptedBytes)
+      val result = JournalRow(
+        persistentRepr = SerializationExtension(system)
+          .deserialize(
+            journalFormat.persistentRepr.toByteArray,
+            classOf[PersistentRepr]
+          )
+          .get,
+        tags = journalFormat.tags.toList
+      )
+      logger.debug("fromBinary:journal = {}", result)
+      result
+    }
+
 }
--- src/main/scala/com/github/j5ik2o/akka/persistence/kafka/serialization/PersistentReprSerializer.scala
+++ src/main/scala/com/github/j5ik2o/akka/persistence/kafka/serialization/PersistentReprSerializer.scala
@@ -4,38 +4,44 @@ import akka.persistence.journal.Tagged
 import akka.persistence.{ AtomicWrite, PersistentRepr }
 import akka.serialization.Serialization
 import com.github.j5ik2o.akka.persistence.kafka.journal.JournalRow
-import com.github.j5ik2o.akka.persistence.kafka.utils.EitherSeq
 
-import scala.util.{ Failure, Success }
+import scala.concurrent.{ ExecutionContext, Future }
 
 object PersistentReprSerializer {
   type JournalWithByteArray = (JournalRow, Array[Byte])
-
 }
 
 class PersistentReprSerializer(serialization: Serialization) {
   import PersistentReprSerializer._
 
-  def serialize(persistentRepr: PersistentRepr): Either[Throwable, JournalWithByteArray] =
+  private val className = classOf[JournalAkkaSerializer].getName
+  private val serializer: JournalAkkaSerializer =
+    serialization
+      .serializerOf(className)
+      .map(_.asInstanceOf[JournalAkkaSerializer])
+      .getOrElse(throw new ClassNotFoundException(className))
+
+  def serialize(
+      persistentRepr: PersistentRepr
+  )(implicit ec: ExecutionContext): Future[JournalWithByteArray] =
     serialize(persistentRepr, None)
 
   def serialize(
       persistentRepr: PersistentRepr,
       tags: Set[String],
       index: Option[Int]
-  ): Either[Throwable, JournalWithByteArray] = {
+  )(implicit ec: ExecutionContext): Future[JournalWithByteArray] = {
     val journal = JournalRow(
       persistentRepr = persistentRepr,
       tags = tags.toSeq,
       ordering = index
     )
-    serialization.serialize(journal).map((journal, _)) match {
-      case Failure(ex)    => Left(ex)
-      case Success(value) => Right(value)
-    }
+    serializer.toBinaryAsync(journal).map((journal, _))
   }
 
-  def serialize(persistentRepr: PersistentRepr, index: Option[Int]): Either[Throwable, JournalWithByteArray] = {
+  def serialize(persistentRepr: PersistentRepr, index: Option[Int])(
+      implicit ec: ExecutionContext
+  ): Future[JournalWithByteArray] = {
     persistentRepr.payload match {
       case Tagged(payload, tags) =>
         serialize(persistentRepr.withPayload(payload), tags, index)
@@ -46,12 +52,12 @@ class PersistentReprSerializer(serialization: Serialization) {
 
   def serialize(
       atomicWrites: Seq[AtomicWrite]
-  ): Seq[Either[Throwable, Seq[JournalWithByteArray]]] = {
+  )(implicit ec: ExecutionContext): Seq[Future[Seq[JournalWithByteArray]]] = {
     atomicWrites.map { atomicWrite =>
       val serialized = atomicWrite.payload.zipWithIndex.map {
         case (v, index) => serialize(v, Some(index))
       }
-      EitherSeq.sequence(serialized)
+      Future.sequence(serialized)
     }
   }
 }
